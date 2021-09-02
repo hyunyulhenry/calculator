ffe1dd8b0fa40e957f8286367be39092c29b823e
hyunyulhenry <leebisu@gmail.com>
2021-09-02 14:46:08 +0900

    upload

--- app.R
+++ app.R
@@ -394,31 +394,6 @@ server <- function(input, output, session) {
     screenshot()
   })
   
-  output$downloadreport <- downloadHandler(
-    # For PDF output, change this to "report.pdf"
-    filename = "report.pdf",
-    content = function(file) {
-      # Copy the report file to a temporary directory before processing it, in
-      # case we don't have write permissions to the current working dir (which
-      # can happen when deployed).
-      tempReport <- file.path(tempdir(), "report.Rmd")
-      file.copy("report.Rmd", tempReport, overwrite = TRUE)
-      library(rmarkdown)
-      # Set up parameters to pass to Rmd document
-      params <- list(input$tp)
-      # Knit the document, passing in the `params` list, and eval it in a
-      # child of the global environment (this isolates the code in the document
-      # from the code in this app).
-      rmarkdown::render(tempReport, pdf_document(),
-                        #output_options=self_contained,
-                        #params = params,
-                        envir = new.env(parent = globalenv())
-      )
-      
-    }
-  )
-  
-  
   
 }
 
