01d1545416a543114d3a3e09405f250bb6da0646
hyunyulhenry <leebisu@gmail.com>
2021-09-03 19:24:33 +0900

    add ignore

--- README.md
+++
@@ -1,3 +0,0 @@
-# 당신의 인생 설계를 계산해 보세요!
-
- https://doomoolmori.shinyapps.io/calculator/
--- script.js
+++
@@ -1,3 +0,0 @@
-function get_id(clicked_id) {
-  Shiny.setInputValue("current_id", clicked_id, {priority: "event"});
-}
